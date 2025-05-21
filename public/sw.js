24052796fc2a7882bffe1bc1de283792b4e6fa28
enricopro <enrico.dalberton@studenti.unipd.it>
2025-05-21 16:16:53 +0200

    update service worker

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,11 @@
 self.addEventListener("push", function (event) {
-  const data = event.data?.json() || {};
+  let data = {};
+  try {
+    data = event.data?.json() || {};
+  } catch (e) {
+    console.error("Push data is not valid JSON", e);
+  }
+
   self.registration.showNotification(data.title || "📸 Time to post!", {
     body: data.body || "Open BeFriends and post your daily photo!",
     icon: "/logo_icon.png",
