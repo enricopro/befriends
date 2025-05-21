7a7e954ab29054646c83d0209ae4575d1bb1bdab
enricopro <enrico.dalberton@studenti.unipd.it>
2025-05-21 17:45:58 +0200

    Rollback sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,7 @@
 self.addEventListener("push", function (event) {
   const data = event.data?.json() || {};
-
-  event.waitUntil( // 🔁 This keeps the SW alive
-    self.registration.showNotification(data.title || "📸 Time to post!", {
-      body: data.body || "Open BeFriends and post your daily photo!",
-      icon: "/logo_icon.png",
-      data: {
-        url: data.data?.url || "https://befriends-jet.vercel.app/post"
-      }
-    })
-  );
+  self.registration.showNotification(data.title || "📸 Time to post!", {
+    body: data.body || "Open BeFriends and post your daily photo!",
+    icon: "/logo_icon.png",
+  });
 });
