a9fb6e418c1be3bf7362c04cf04b1e41e633f382
enricopro <enrico.dalberton@studenti.unipd.it>
2025-05-21 16:00:41 +0200

    Update iOS detection

--- src/components/Post/DualCameraCapture.tsx
+++ src/components/Post/DualCameraCapture.tsx
@@ -14,9 +14,7 @@ const DualCameraCapture = ({ onCapture }: Props) => {
 
   // Detect iOS
   useEffect(() => {
-    alert(window.navigator.userAgent);
-    alert("isIos: " + /iPad|iPhone|iPod/.test(navigator.userAgent))
-    const isIos = /iPad|iPhone|iPod/.test(navigator.userAgent);
+    const isIos = /iPad|iPhone|iPod|Safari/.test(navigator.userAgent);
     if (isIos) {
       setUseFallback(true);
     }
